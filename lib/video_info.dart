c5645e8d08238a89f69806e93e738ceea48d72b1
Maxim Barabas <maximbarabas@Maxim-MacBook-Air.local>
2023-05-11 00:37:31 +0200

    second page first row

--- lib/video_info.dart
+++ lib/video_info.dart
@@ -16,9 +16,37 @@ class _VideoInfoState extends State<VideoInfo> {
     return Scaffold(
         body: Container(
             decoration: BoxDecoration(
-                gradient: LinearGradient(colors: [
-      color.AppColor.gradientFirst,
-      color.AppColor.gradientSecond
-    ]))));
+                gradient: LinearGradient(
+              colors: [
+                color.AppColor.gradientFirst.withOpacity(0.9),
+                color.AppColor.gradientSecond
+              ],
+              begin: const FractionalOffset(0.0, 0.4),
+              end: Alignment.topRight,
+            )),
+            child: Column(
+              children: [
+                Container(
+                    padding:
+                        const EdgeInsets.only(top: 70, left: 30, right: 30),
+                    width: MediaQuery.of(context).size.width,
+                    height: 300,
+                    child: Column(
+                      children: [
+                        Row(
+                          children: [
+                            Icon(Icons.arrow_back_ios,
+                                size: 20,
+                                color: color.AppColor.secondPageIconColor),
+                            Expanded(child: Container()),
+                            Icon(Icons.info_outline,
+                                size: 20,
+                                color: color.AppColor.secondPageIconColor)
+                          ],
+                        )
+                      ],
+                    ))
+              ],
+            )));
   }
 }
